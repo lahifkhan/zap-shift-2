394a5be7033c69c765afaab6e91d9a5eb23bc2d0
lahifkhan <lahifkhan52@gmail.com>
2026-03-29 23:08:03 +0600

    swiper not working issue fixed

--- src/Pages/Home/Brand.jsx
+++ src/Pages/Home/Brand.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import * as Marquee from "react-fast-marquee";
+import { Autoplay, FreeMode } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 // brands img
 import brand1 from "../../assets/brands/amazon.png";
@@ -15,17 +22,36 @@ const brands = [brand1, brand2, brand3, brand4, brand5, brand6, brand7];
 const Brand = () => {
   return (
     <div>
-      <h3 className="text-[28px] font-extrabold text-center text-[#03373D]">
+      <h3 className="text-[28px] font-extrabold text-center text-[#03373D] mb-6">
         We've helped thousands of sales teams
       </h3>
 
-      <Marquee.default>
+      <Swiper
+        modules={[Autoplay, FreeMode]}
+        spaceBetween={30}
+        slidesPerView="auto"
+        loop={true}
+        speed={8000}
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false, // keeps running after user interaction
+          pauseOnMouseEnter: false, // don't pause on hover
+        }}
+        freeMode={{
+          enabled: true,
+          momentum: false, // ← this is the main fix, stops the slowdown/speed-up effect
+        }}
+      >
         {brands.map((brd, index) => (
-          <div key={index} className="mx-8">
-            <img src={brd} alt="brand" />
-          </div>
+          <SwiperSlide key={index} style={{ width: "auto" }}>
+            <img
+              src={brd}
+              alt="brand"
+              style={{ height: "60px", width: "60" }}
+            />
+          </SwiperSlide>
         ))}
-      </Marquee.default>
+      </Swiper>
     </div>
   );
 };
